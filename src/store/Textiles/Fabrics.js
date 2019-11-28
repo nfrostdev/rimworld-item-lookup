5e5fd628d02f4d413f99ac12e8115f9c91d38e74
nickfrostus <nickfrost.us@gmail.com>
2019-11-27 21:32:30 -0500

    added fabrics

--- src/store/Textiles/Fabrics.js
+++ src/store/Textiles/Fabrics.js
@@ -1,23 +1,185 @@
 export default [
     {
-        name: 'Cloth',
-        description: 'The strong furry leather of a bear. Compared to most leather, it is quite a bit tougher, and a bit better at all kinds of temperature regulation.',
+        name: 'Alpaca Wool',
+        description: 'The remarkably soft wool of an alpaca. It is very warm.',
         beauty: -4,
-        deterioration_rate: 2,
-        flammability: 1,
-        hp: 60,
-        value: 3.4,
+        deterioration_rate: 2.6,
+        flammability: 0.4,
+        hp: 90,
+        value: 2.7,
         mass: 0.03,
         multipliers: {
             hp: 1.3,
             armor: {
-                sharp: 1.12,
-                blunt: 0.24,
-                heat: 1.5,
+                sharp: 0.36,
+                blunt: 0,
+                heat: 1.1,
             },
+            flammability: 1.7,
             insulation: {
-                cold: 36,
-                heat: 36
+                cold: 50.4,
+                heat: 28.8
+            },
+        },
+    },
+    {
+        name: 'Camelhair',
+        description: 'Camelhair clothing is usually worn by desert travelers to protect them from the heat. It provides excellent heat insulation due to its thermostatic properties.',
+        beauty: -4,
+        deterioration_rate: 3.4,
+        flammability: 0.4,
+        hp: 90,
+        value: 2.7,
+        mass: 0.03,
+        multipliers: {
+            hp: 1.3,
+            armor: {
+                sharp: 0.36,
+                blunt: 0,
+                heat: 1.1,
+            },
+            flammability: 1.7,
+            insulation: {
+                cold: 32.4,
+                heat: 46.8
+            },
+        },
+    },
+    {
+        name: 'Cloth',
+        description: 'Cloth woven from organic fibers.',
+        beauty: -4,
+        deterioration_rate: 4,
+        flammability: 1.2,
+        hp: 80,
+        value: 1.5,
+        mass: 0.03,
+        multipliers: {
+            hp: 1.3,
+            armor: {
+                sharp: 0.36,
+                blunt: 0,
+                heat: 0.18,
+            },
+            flammability: 1.2,
+            insulation: {
+                cold: 32.4,
+                heat: 32.4
+            },
+        },
+    },
+    {
+        name: 'Devilstrand',
+        description: 'Fabric spun from microfibers extracted from devilstrand mushrooms. It is very tough, good at insulating, and protects exceptionally well against flame.',
+        beauty: -4,
+        deterioration_rate: 3,
+        flammability: 0.4,
+        hp: 100,
+        value: 5.5,
+        mass: 0.03,
+        multipliers: {
+            hp: 1.3,
+            armor: {
+                sharp: 1.4,
+                blunt: 0.36,
+                heat: 3,
+            },
+            flammability: 0.4,
+            insulation: {
+                cold: 36.0,
+                heat: 43.2
+            },
+        },
+    },
+    {
+        name: 'Hyperweave',
+        description: 'Nano-fibers woven into sheets molecule-by-molecule using specialized production mechanites. While it flexes freely most of the time, it stiffens to absorb blows as they land, and is incredibly difficult to break. The secrets of its production are know only to the most advanced glitterworld cultures.',
+        beauty: -4,
+        deterioration_rate: 1,
+        flammability: 0.4,
+        hp: 200,
+        value: 9,
+        mass: 0.04,
+        multipliers: {
+            hp: 1.3,
+            armor: {
+                sharp: 2,
+                blunt: 0.54,
+                heat: 2.88,
+            },
+            flammability: 0.4,
+            insulation: {
+                cold: 46.8,
+                heat: 46.8
+            },
+        },
+    },
+    {
+        name: 'Megasloth Wool',
+        description: 'While somewhat coarse, megasloth wool\'s long fibers make it very strong. It is exceptionally warm.',
+        beauty: -4,
+        deterioration_rate: 3.4,
+        flammability: 0.4,
+        hp: 90,
+        value: 2.7,
+        mass: 0.03,
+        multipliers: {
+            hp: 1.3,
+            armor: {
+                sharp: 0.8,
+                blunt: 0,
+                heat: 1.1,
+            },
+            flammability: 1.7,
+            insulation: {
+                cold: 61.2,
+                heat: 21.6
+            },
+        },
+    },
+    {
+        name: 'Muffalo Wool',
+        description: 'Soft, light, and provides very good insulation. Muffalo wool clothes can keep the body warm even in very cold conditions.',
+        beauty: -4,
+        deterioration_rate: 3.4,
+        flammability: 0.4,
+        hp: 90,
+        value: 2.7,
+        mass: 0.03,
+        multipliers: {
+            hp: 1.3,
+            armor: {
+                sharp: 0.36,
+                blunt: 0,
+                heat: 1.1,
+            },
+            flammability: 1.7,
+            insulation: {
+                cold: 54,
+                heat: 21.6
+            },
+        },
+    },
+    {
+        name: 'Synthread',
+        description: 'Spacer-tech synthetic fabric designed for making ordinary clothes. It is soft, light, strong, and insulates well.',
+        beauty: -4,
+        deterioration_rate: 3,
+        flammability: 0.7,
+        hp: 150,
+        value: 4,
+        mass: 0.03,
+        multipliers: {
+            hp: 1.3,
+            armor: {
+                sharp: 0.94,
+                blunt: 0.26,
+                heat: 0.9,
+            },
+            flammability: 0.7,
+            insulation: {
+                cold: 39.6,
+                heat: 39.6
             },
         },
     },
